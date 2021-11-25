5c5928f349854487719ea2c3ddc28dab95dfa8b0
afilazzola <alex.filazzola@outlook.com>
2021-11-25 17:09:35 -0500

    processing the polygons within the available datasets

--- scripts/CHspatial.R
+++ scripts/CHspatial.R
@@ -21,3 +21,31 @@ trails <- readOGR(layer="Trails", dsn="data//CHProperties")
 
 ## find polygon intersection with sites
 out <- st_intersection(sites, lands)
+
+## Properties with data
+studyAreas <- lands[lands$Name %in% out$Name,]
+
+
+#### Connect Mapbox polygons
+mapbox <- read.csv("data//Mapbox//mapboxJunJulyAug2020.csv")
+e <- as(raster::extent(78.46801, 78.83157, 19.53407, 19.74557), "SpatialPolygons")
+
+outPoly <- data.frame()
+for(i in 1:nrow(mapbox)){
+  ### load mapbox polygon
+  bbox <- as.numeric(strsplit(mapbox[i,"bounds"], split=",")[[1]]) ## extract boundaries
+  e <- as(raster::extent(bbox[1],bbox[3],bbox[2],bbox[4]), "SpatialPolygons") ## convert to polygon
+  proj4string(e) <- "+proj=longlat +datum=WGS84 +no_defs" ## assign CRS
+  e <- st_as_sf(e) ## convert to SF data class
+  mapboxMatch <- st_intersection(studyAreas, e)
+  if(nrow(mapboxMatch)==0){
+    next
+  } else{
+  polyData <- mapbox[i,]
+  }
+  outPoly <- rbind(outPoly, polyData)
+  print(i)
+}
+
+
+
