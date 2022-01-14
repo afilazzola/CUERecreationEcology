d18061e6bb2785b974e7d4836ac2f2fb0bb4fffd
afilazzola <alex.filazzola@outlook.com>
2022-01-14 17:03:19 -0500

    finished preliminary exploration of mapbox patterns

--- scripts/mapboxViz.r
+++ scripts/mapboxViz.r
@@ -9,26 +9,22 @@ mapbox <- read.csv("out//data//MapboxSummaryData.csv")
 ## Functions
 source("scripts/functions.r")
 
-ggplot(mapbox, aes(x=dayOfWeek, y=avgLogActivity, fill=accessibility)) +
+plot1 <- ggplot(mapbox, aes(x=accessibility, y=avgLogActivity, fill=dayOfWeek)) +
     geom_boxplot() + theme_classic() +
     scale_fill_manual(values=c("#E69F00", "#56B4E9")) + 
     xlab("") + ylab("Average Mobile Activity (log-transformed)") + 
-    theme(text = element_text(size = 16)) 
-save_PDF(plotName = "meanPatterns.pdf")
+    theme(text = element_text(size = 16), legend.position = c(0.15, 0.9))
+plot1
 
 ggplot(mapbox, aes(x = reorder(Name, avgLogActivity), y=avgLogActivity)) +
  geom_boxplot() + coord_flip() + xlab("") + 
  ylab("Average Mobile Activity (log-transformed)") + theme_classic() +
  theme(text = element_text(size = 16))
-save_PDF(plotName = "PropertyActivity.pdf", setHeight = 10)
+
 
 ggplot(mapbox %>% distinct(Name, HumanMobilePercent), aes(x = reorder(Name, HumanMobilePercent), y = HumanMobilePercent)) +
  geom_bar(stat="identity") + coord_flip() + xlab("") + 
  ylab("Area with human activity (percent)") + theme_classic()
- save_PDF(plotName = "HumanActivity.pdf", setHeight = 10)
-
-
-
 
 
 ## Load reservation data to explore with mapbox
@@ -55,18 +51,14 @@ lmOut <- effects::effect("dailyAdults", m1,
     xlevels = list(dailyAdults = seq(150,550,50))) %>% 
     data.frame()
 
-ggplot(mapboxParkReservations %>% filter(Name != "Mountsberg"),
+plot2 <- ggplot(mapboxParkReservations %>% filter(Name != "Mountsberg"),
  aes(x = dailyAdults, y =avgLogActivity, color=dayOfWeek, label = Name)) +
  scale_color_manual(values=c("#E69F00", "#56B4E9")) + theme_classic() +
  geom_line(data=  lmOut ,aes(x  = dailyAdults, y= fit, label = NA), color = "grey40", size=1.6) +
  geom_text() + xlim(100, 550) + xlab("Total daily reservations") + 
  ylab("Average Mobile Activity (log-transformed)")  +
- theme(text = element_text(size = 16)) 
-  save_PDF(plotName = "reservationPatterns.pdf")
-
-ggplot(mapboxParkReservations, aes(x = dailyYouths, y = avgLogActivity)) +
- geom_point()
-
+ theme(text = element_text(size = 16), legend.position = c(0.15, 0.9)) 
+plot2
 
 
  ### Trail density comparisons with mapbox
@@ -97,15 +89,16 @@ lm2Out <- effects::effect("poly(trailLengthkm2,2)", m2,
     data.frame()
 
 
-ggplot(mapboxTrails %>% filter(accessibility == "open"),
+plot3 <- ggplot(mapboxTrails %>% filter(accessibility == "open"),
     aes(x = trailLengthkm2, y = avgLogActivity, color = dayOfWeek, label=Name)) +
 geom_text() +
 scale_color_manual(values=c("#E69F00", "#56B4E9")) + theme_classic() +
 xlab("Total Length of Trails (km)") +
  ylab("Average Mobile Activity (log-transformed)") +
  xlim(-3,30) +
- geom_line(data=  lm2Out ,aes(x  = trailLengthkm2, y= fit, label = NA), color = "grey40", size=1.6) 
-
+ theme(text = element_text(size = 16), legend.position = c(0.15, 0.9)) +
+ geom_line(data=  lm2Out ,aes(x  = trailLengthkm2, y= fit, label = NA), color = "grey60", size=1.2) 
+plot3
 
 ProportionActivityData <- mapboxTrails %>% 
     filter(accessibility == "open" & dayOfWeek == "weekend") %>% 
@@ -121,12 +114,72 @@ lm3Out <- effects::effect("trailDensity", m3,
     data.frame()
 
 
-ggplot(ProportionActivityData,
+plot4 <- ggplot(ProportionActivityData,
     aes(x = trailDensity, y = humanMobileProp*100,  label = Name)) +
-geom_text() +
+geom_text() +   theme(text = element_text(size = 16))  +
  theme_classic() +
 xlab("Trail Density (km/km2)") +
  ylab("Percent area with any human activity") +
  xlim(0,8) + ylim(20,100) +
- geom_line(data=  lm3Out ,aes(x  = trailDensity, y= fit*100, label = NA), color = "grey40", size=1.6) 
+ theme(text = element_text(size = 16)) +
+ geom_line(data=  lm3Out ,aes(x  = trailDensity, y= fit*100, label = NA), color = "grey60", size=1.2) 
+plot4
 
+ggsave("WeekendPatterns.pdf", arrangeGrob(plot3, plot4, ncol=2), width=11, height=5.5)
+
+
+
+#### Exploration of Mapbox data on its own
+dayOfWeekTest <- mapbox %>% 
+            dplyr::select(Name, dayOfWeek, accessibility, avgLogActivity) %>% 
+            spread(dayOfWeek, avgLogActivity)
+           
+
+plot6 <- ggplot(dayOfWeekTest %>% filter(accessibility == "open"), aes(x=weekday, y = weekend)) +
+    geom_point() + theme_classic() +
+     ylab("Weekend activity") +
+    xlab("Weekday activity") +
+    theme(text = element_text(size = 16)) +
+    geom_smooth(method="lm", color="grey60", se=F, size=1.2)
+
+AccessibilityTest <- mapbox %>% 
+            dplyr::select(Name, dayOfWeek, accessibility, avgLogActivity) %>% 
+            spread(accessibility, avgLogActivity) 
+
+plot7 <-  ggplot(AccessibilityTest, aes(x=open, y = closed, color = dayOfWeek)) +
+    geom_point() + theme_classic() + scale_color_manual(values=c("#E69F00", "#56B4E9")) +
+    ylab("Closed hours activity") +
+    xlab("Open hours activity") +
+    theme(text = element_text(size = 16), legend.position = c(0.15, 0.9)) 
+
+
+m4 <- lm(TrailActivityPercent ~ HumanMobilePercent, data=mapbox)
+summary(m4)
+
+## Comparison of on trail use to general human mobile
+
+plot5 <- ggplot(mapbox, aes(x=TrailActivityPercent, y = HumanMobilePercent, label = Name)) + 
+ theme_classic() +ylim(20,100) + xlim(-10,80) + 
+xlab("Percent of activity directly on trails") + 
+ylab("Percent area with any human activity") +  
+ theme(text = element_text(size = 16)) +
+ geom_smooth(method = "lm", color="grey60", se=F, size=1.2,fullrange=TRUE) +
+ geom_text()
+plot5
+
+## Activity of land management
+parkType <- mapbox %>% group_by(Area_Type, Managed ) %>% 
+    summarize(nSites = length(unique(Name)),
+            PercentHumanActivity = mean(HumanMobilePercent),
+            AvgSize = mean(PropertyAreakm2))
+parkType
+write.csv(parkType, "figs//Table1.csv", row.names=F)
+
+## Save output plots
+ggsave("figs/WeekendPatterns.pdf", arrangeGrob(plot1, plot2), height=13)
+ggsave("figs/TrailPatterns.pdf", 
+    arrangeGrob(plot3, plot4, plot5), 
+    height=14, width=8)
+ggsave("figs/AccessPatterns.pdf", 
+    arrangeGrob(plot6, plot7), 
+    height=10, width=8)
